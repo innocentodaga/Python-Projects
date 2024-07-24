3e78c4752d474d636601dd1758caf420fbd0db26
tys0n <101554587+innocentodaga@users.noreply.github.com>
2024-07-24 11:16:47 +0300

    Comic Creator

---
+++ Comic Creator/drawing.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+
+class DrawingSpace(RelativeLayout):
+    pass
+
+class DrawingApp(App):
+    def build(self):
+        return DrawingSpace()
+
+
+if __name__ == "__main__":
+    DrawingApp().run()
